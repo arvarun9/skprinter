a9e66443b3483b2f83fe49000b73c8dfa89e9c5f
varun <arvarun93@gmail.com>
2021-12-20 15:55:45 +0530

    all changes made

---
+++ query.js
@@ -0,0 +1,15 @@
+ $(function() {
+    $('#prod').click(function() {
+      $('html,body').animate({scrollTop:$('#Products').offset().top},600)
+    }); 
+  })
+  $(function() {
+    $('#print').click(function() {
+      $('html,body').animate({scrollTop:$('#printing-slide').offset().top},1000)
+    }); 
+  })
+  $(function() {
+    $('#cont').click(function() {
+      $('html,body').animate({scrollTop:$('#contact').offset().top},1500)
+    }); 
+  })
